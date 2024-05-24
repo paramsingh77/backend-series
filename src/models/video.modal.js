668c0cb964ee6c074e701656468d5c14bd67df47
[Parminder SIngh] <113473052+paramsingh77@users.noreply.github.com>
2024-05-24 16:37:04 -0500

    User and Video modal completed

---
+++ src/models/video.modal.js
@@ -0,0 +1,43 @@
+import mongoose, { Mongoose, Schema } from "mongoose";
+import mongooseAggregatepaginate from "mongoose-aggregate-paginate-v2";
+
+
+const videModal  = new mongoose.Schema({
+    videoFile:{
+        type:String, //cloudinary url
+        required:true,
+    },
+    thumbnail:{
+        type:String, //cloudinary url
+        required:true,
+    },
+    title:{
+        type:String, 
+        required:true,
+    },
+    description:{
+        type:String, 
+        required:true,
+    },
+    duration:{
+        type:Number, //cloudinary time
+        required:true,
+    },
+    view:{
+        type:Number,
+        default:0,
+    },
+    isPublished:{
+        type:Boolean,
+        default:true,
+    },
+    owner:{
+        type:Schema.Types.ObjectId,
+        ref:"User",
+    }
+
+},{timestamps:true})
+
+videoSchema.plugin(mongooseAggregatepaginate)
+
+export const Video = mongoose.model('Video',videModal)
