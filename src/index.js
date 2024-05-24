e17ed1332597a4a3579059389d1752d3e871fa1f
[Parminder SIngh] <113473052+paramsingh77@users.noreply.github.com>
2024-05-24 11:19:05 -0500

    utils

--- src/index.js
+++ src/index.js
@@ -1,11 +1,17 @@
 // require('dotenv').config({path: './env'})
 import dotenv from "dotenv"
 import connectDB from "./db/index.js";
-// import {app} from './app.js'
+import {app} from './app.js'
 dotenv.config({
     path: './.env'
 })
 
+const ports = process.env.PORT || 8000 
 
-
-connectDB()
+connectDB().then(()=>{
+    app.listen(ports , ()=>{
+        console.log(`Server connected at ${ports}`);
+    })
+}).catch((error)=>{
+    console.log("MONGO db connection error",error);
+})
