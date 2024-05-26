8a77de10647868d3b206236924956baa7d5e3854
[Parminder SIngh] <113473052+paramsingh77@users.noreply.github.com>
2024-05-25 21:15:28 -0500

    implemented registered user

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,9 +1,68 @@
 import { asyncHandler } from "../utils/asyncHandler.js";
+import {ApiError} from "../utils/ApiError.js"
+import {User} from "../models/user.modal.js"
+import {uploadOnCloudinary} from "../utils/cloudinary.js"
+import {ApiResponse} from "../utils/ApiResponse.js"
 
 const registerUser = asyncHandler( async (req,res) =>{
-    res.status(200).json({
-        message:"Done"
+    // Getting details user from frontend
+    // Validation - not empty
+    // Check if user already exist username , email
+    // check for images . check for avatar,
+    // Upload them cloudinary
+    // create user object - create entry in db
+    // Remove password and remove fresh token field form response.
+    // check for user creation
+    // return response
+    
+    const {fullName , email , userName , password} = req.body
+    console.log("email:" , email);
+
+    if(
+        [fullName,email,userName,password].some((filed)=>filed?.trim() ==="")
+    ){
+        throw new ApiError(400,"All Fields are compulsory")
+    }
+    const existedUser = User.findOne({
+        $or: [{ userName }, { email }]
     })
+    if(existedUser){
+        throw new ApiError(409, "User with email or username already exist")
+    }
+
+    const avatarLocalPath = req.files?.avatar[0]?.path;
+    const coverImageLocalPath =  req.files?.coverImage[0]?.path;
+
+    if(!avatarLocalPath){
+        throw new ApiError(400,"Avatar is required")
+    }
+    
+    //uploading on cloudinary
+   const avatar =  await uploadOnCloudinary(avatarLocalPath) 
+   const coverImage = await uploadOnCloudinary(coverImageLocalPath)
+
+   if(!avatar){
+    throw new ApiError(400 , "Avatar file is required")
+   }
+
+   await User.create({
+    fullName,
+    avatar: avatar.url,
+    coverImage: coverImage?.url ||"",
+    email,
+    password,
+    userName: userName.toLowercase()
+   })
+
+   const createdUser = await User.findById(user._id).select(
+    "-password -refreshToken"
+   )
+   if(!createdUser){
+            throw new ApiError(500,"Something went wrong while registering the user")
+   }
+   return res.status(201).json(
+    new ApiResponse(200, createdUser, "User registered successfully")
+   )
 } )
 
 export {registerUser}
--- src/models/user.modal.js
+++ src/models/user.modal.js
@@ -50,7 +50,7 @@ const userModal = new mongoose.Schema({
 
 userModal.pre("save",async function(next){
     if(!this.isModified("password")) return next()
-    this.password = bcrypt.hash(this.password , 10)
+    this.password = await bcrypt.hash(this.password , 10)
     next()
 })
 
